293f376a05c45d595181dff47c63ee35530aff2c
itc-s24025 <s24025@std.it-college.ac.jp>
2026-02-03 09:37:57 +0900

    ホームのUIの配置のみ行った。

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -4,9 +4,10 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from "@/app/lib/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import GeminiIntegration from "./_components/gemini/GeminiInteraction";
-import LogoutButton from "@/app/_components/GoogleLogoutButton";
-import LoginButton from "@/app/_components/GoogleLoginButton";
+// import GeminiIntegration from "./_components/gemini/GeminiInteraction";
+// import LogoutButton from "@/app/_components/GoogleLogoutButton";
+// import LoginButton from "@/app/_components/GoogleLoginButton";
+import ChatPage from "./chat/page";
 
 export default function Home() {
     const [user, loading] = useAuthState(auth);
@@ -14,35 +15,25 @@ export default function Home() {
 
     // ユーザーが未ログインの場合、ログインページへリダイレクト
     useEffect(() => {
-        if (!loading && !user) {
-            router.replace("/login");
-        }
+        if (loading) return // ローディング中は何もしない
+        if (!user) router.replace("/login");
     }, [loading, user, router]);
 
-    if (loading) {
-        return <p>読み込み中...</p>;
-    }
+    if (loading) return <p>読み込み中...</p>;
+    if (!user) return null; // リダイレクト中
 
-    // if (!user) {
-    //     return (
-    //         <>
-    //             <p>ログインしてください</p>
-    //             <LoginButton />
-    //         </>
-    //     )
-    // }
-
-    return (
-        <main>
-            <h1>言い訳を生成</h1>
-            <p>
-                hello：
-                <strong>{user?.displayName}</strong>
-            </p>
-
-            <p>メールアドレス：{user?.email}</p>
-            <GeminiIntegration />
-            <LogoutButton />
-        </main>
-    );
+    return <ChatPage />;
+    // return (
+    //     <main>
+    //         <h1>言い訳を生成</h1>
+    //         <p>
+    //             hello：
+    //             <strong>{user?.displayName}</strong>
+    //         </p>
+    //
+    //         <p>メールアドレス：{user?.email}</p>
+    //         <GeminiIntegration />
+    //         <LogoutButton />
+    //     </main>
+    // );
 }
